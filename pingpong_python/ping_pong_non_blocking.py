3092ba3f679947572f6663f0882d260cfdbf3fcb
DavidButts <davidbutts96@gmail.com>
2019-03-24 13:48:53 -0400

    non blocking code and makes a plot

--- pingpong_python/ping_pong_non_blocking.py
+++ pingpong_python/ping_pong_non_blocking.py
@@ -8,32 +8,29 @@ comm = MPI.COMM_WORLD
 size = comm.size
 rank = comm.rank
 
-def trial(steps,size):
-    mess = np.ones((size),dtype='d')
-    print(mess.shape)
-    tot_time = 0
 
+def trial(steps,size):
+    mess_in = np.empty((size),dtype=np.float64)
+    mess_out = np.ones((size),dtype=np.float64)
     for t in range(steps):
         start = MPI.Wtime()
-		
         if rank == 0:
-        	comm.Isend(mess, dest = 1, tag = 0)
-        	req = comm.Irecv(mess, source = 1, tag = 1)
+            req = comm.Isend([mess_out, MPI.DOUBLE], dest = 1, tag = 0)
+            comm.Recv([mess_in, MPI.DOUBLE], source = 1, tag = 1)
         if rank == 1:
-            req = comm.Irecv(mess, source = 0, tag = 0)
-            comm.isend(mess, dest = 0, tag = 1)
-
+            comm.Recv([mess_in, MPI.DOUBLE], source = 0, tag = 0)
+            req = comm.Isend([mess_out, MPI.DOUBLE], dest = 0, tag = 1)
+        req.Wait()
         end = MPI.Wtime()
-        tot_time += end-start
-
-        return tot_time/steps
+        tot_time = end - start
+    return tot_time/steps
 
 data = []
 for n in range(0,25,2):
-    t = trial(50000,2**n)
+    t = trial(10,2**n)
     if rank == 0:
         data.append(t)
-        # print('n =',2**n)
+        print('n =',2**n)
         # print('Time per send-recv:',t)
 if rank == 0:
     plt.loglog(range(0,25,2),data)
